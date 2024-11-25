f5629b1fd7265c4879fd8fc780310cd7c784b68f
Poleac Liviu Stefan <liviu-stefan.poleac@s.unibuc.ro>
2024-11-26 01:46:06 +0200

    Fixed Widget

--- storefront/src/app/(components)/instagramWidget/InstagramWidget.tsx
+++ storefront/src/app/(components)/instagramWidget/InstagramWidget.tsx
@@ -1,34 +1,22 @@
-'use client'
 import { useEffect } from "react";
 
 const InstagramWidget = () => {
   useEffect(() => {
-    // Adaugă scriptul Elfsight
+    // Adaugă script-ul doar o dată
     const script = document.createElement("script");
     script.src = "https://static.elfsight.com/platform/platform.js";
     script.async = true;
     document.body.appendChild(script);
 
-    // Elimină branding-ul după ce widget-ul s-a încărcat
-    const removeBranding = () => {
-      const elfsightBranding = document.querySelector("a[href*='elfsight']");
-      if (elfsightBranding) {
-        elfsightBranding.style.display = "none";
-      }
-    };
-
-    // Asigură-te că branding-ul este eliminat periodic, în caz că este re-adăugat
-    const intervalId = setInterval(removeBranding, 1000);
-
     return () => {
-      clearInterval(intervalId);
+      // Curăță script-ul dacă este necesar
       document.body.removeChild(script);
     };
   }, []);
 
   return (
     <div
-      className="elfsight-app-f21305b2-a3fe-4667-8642-78bd065b28a9 w-full lg:!w-[900px]"
+      className="elfsight-app-f21305b2-a3fe-4667-8642-78bd065b28a9"
       data-elfsight-app-lazy
     ></div>
   );
