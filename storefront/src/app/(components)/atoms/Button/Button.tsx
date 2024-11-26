87a6171e2d4d071fd1a215294d0af9827203f087
Poleac Liviu Stefan <liviu-stefan.poleac@s.unibuc.ro>
2024-11-26 11:40:52 +0200

    Fixed button

--- storefront/src/app/(components)/atoms/Button/Button.tsx
+++ storefront/src/app/(components)/atoms/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({children,bg,bgHover,color,colorHover}) =>{
     return (
-        <button className={`bg-${bg ? bg :"black"} text-${color ? color : "white"} transition ease-in-out duration-[.3s] hover:bg-${bgHover ? bgHover : "bg-gray-500"} hover:text-${colorHover ? colorHover : "white"} `}>{children}</button>
+        <button className={`bg-${bg ? bg :"black"} text-${color ? color : "white"} transition ease-in-out duration-300 hover:bg-${bgHover ? bgHover : "bg-gray-500"} hover:text-${colorHover ? colorHover : "white"} `}>{children}</button>
     )
 }
 
