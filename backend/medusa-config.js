d7de1cf7ba38c9b6af44f81c9b4659b613f738bf
Poleac Liviu Stefan <liviu-stefan.poleac@s.unibuc.ro>
2024-12-14 23:01:19 +0200

    Corrected url backend

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -4,7 +4,7 @@ const isDev = process.env.NODE_ENV === 'development';
 
 loadEnv(process.env.NODE_ENV, process.cwd());
 
-const backendUrl = "www.api.lorenalash.ro";
+const backendUrl = "https://www.api.lorenalash.ro" 
 
 const plugins = [
   // 'medusa-fulfillment-manual'
